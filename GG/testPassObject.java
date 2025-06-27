af2e4dc7a4ce085605eda6e652267eba4c2a7e60
hoduchanh2308 <216514568+hoduchanh2308@users.noreply.github.com>
2025-06-27 09:36:45 +0000

    123

--- GG/testPassObject.java
+++ GG/testPassObject.java
@@ -3,20 +3,9 @@ public class testPassObject{
 
 
     public static void testHanh(){
-
-
- 
         Number n = new Number();
-
         n.i=14;
-
         PassObject.f(n);
         System.out.println(n.i);
-    
-
-
-
     }
-
-
 }
