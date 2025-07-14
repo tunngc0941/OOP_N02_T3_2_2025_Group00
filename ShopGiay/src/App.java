cf92c3c29e1d94d6c2e1e08c4456834e37b784be
hoduchanh2308 <216514568+hoduchanh2308@users.noreply.github.com>
2025-07-14 06:57:50 +0000

    Hanh Commit

--- ShopGiay/src/App.java
+++ ShopGiay/src/App.java
@@ -1,12 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        // Tạo khách hàng
         KhachHang kh = new KhachHang("KH001", "Nguyễn Văn A", "0901234567");
-
-        // Tạo sản phẩm
         SanPham sp = new SanPham("SP001", "Giay bup be", "39", 150000, 20);
-
-        // Tạo giao dịch: khách hàng mua 3 sản phẩm
         GiaoDich gd = new GiaoDich(kh, sp, 3);
         gd.hienThiGiaoDich();
     }
