b18c03ca20438ad7b909303c9ad7def9c35b6134
hoduchanh2308 <216514568+hoduchanh2308@users.noreply.github.com>
2025-07-14 08:54:56 +0000

    123456

--- ShopGiay/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        KhachHang kh = new KhachHang("KH001", "Nguyễn Văn A", "0901234567");
-        SanPham sp = new SanPham("SP001", "Giay bup be", "39", 150000, 20);
-        GiaoDich gd = new GiaoDich(kh, sp, 3);
-        gd.hienThiGiaoDich();
-    }
-}
