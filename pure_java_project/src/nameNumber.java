2d427afb092beb5f838e01ca2d2b41c63c2b8553
Tuan Ngoc <tuanngoc0906@gmail.com>
2025-07-17 07:08:01 +0000

    nameNumber

---
+++ pure_java_project/src/nameNumber.java
@@ -0,0 +1,18 @@
+public class nameNumber {
+    private String lastName;
+    private String telNumber;
+    public NameNumber() {};
+
+    public NameNumber(String name, String num) {
+        name = lastName;
+        num = telNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+}
