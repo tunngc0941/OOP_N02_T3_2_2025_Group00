d2c70896b506964317a031762d1f424689b49a5d
Tuan Ngoc <tuanngoc0906@gmail.com>
2025-08-13 16:58:27 +0000

    ngoc

--- backend/src/main/java/com/example/demo/repository/ShoeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Shoe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoeRepository extends JpaRepository<Shoe, Long> {}
